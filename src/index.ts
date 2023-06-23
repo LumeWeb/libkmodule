e4dd9e715cab312dc6a19736f459533f3196cade
Derrick Hammer <derrick@derrickhammer.com>
2023-06-23 04:40:50 -0400

    fix: seed is now key

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { log, logErr } from "./log.js";
 export { ActiveQuery, addHandler, handleMessage } from "./messages.js";
 export { callModule, connectModule, newKernelQuery } from "./queries.js";
-export { getDataFromKernel, getSeed } from "./seed.js";
+export { getDataFromKernel, getKey } from "./key.js";
 export { moduleQuery, presentKeyData } from "./types.js";
 export { DataFn, Err, addContextToErr, objAsString } from "@lumeweb/libweb";
